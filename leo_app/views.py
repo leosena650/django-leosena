ea53ce33f4d8ca833e49618272e18579daded63b
leo <leosena@gmail.com>
2025-07-10 15:34:07 +0800

    Save changes

--- leo_app/views.py
+++ leo_app/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
+
+
 def home(request):
+    if request.user.is_authenticated:
+        return render(request, 'home.html')
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'Login successful!')
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid username or password.')
+            return redirect('home')
     return render(request, 'home.html')
+
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, 'Logged out successfully!')
+    return redirect('home')
